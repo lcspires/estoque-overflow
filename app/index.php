ae4dd5b9087c323218a8ebe8d4d260f6c9d696d0
Lucas Pires <lucasopf95@gmail.com>
2024-12-21 22:11:42 -0300

    starting web app

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LCS Inventory</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+    echo "<h1>Welcome to the LCS Inventory!</h1>";
+    ?>
+	<script src="scripts.js"></script>
+</body>
+</html>
